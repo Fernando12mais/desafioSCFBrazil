042cb56891c72f68fc67d4b8c5e7106ab4545aed
fernando12mais <fernandogabriel12mais@gmail.com>
2023-06-11 15:36:24 -0300

    feat:Adds more users to fake database

--- fakeData.js
+++ fakeData.js
@@ -1,11 +1,34 @@
+const fakeData = [
+  {
+    id: 1,
+    name: "João Oliveira",
+    job: "Desenvolvedor",
+    accessedCount: 0,
+    permissions: {
+      erase: false,
+      update: false,
+    },
+  },
+  {
+    id: 2,
+    name: "Marcelo Dias",
+    job: "Desenvolvedor",
+    accessedCount: 0,
+    permissions: {
+      erase: false,
+      update: false,
+    },
+  },
+  {
+    id: 3,
+    name: "Mônica Silveira",
+    job: "Desenvolvedor",
+    accessedCount: 0,
+    permissions: {
+      erase: false,
+      update: false,
+    },
+  },
+];
 
-const fakeData  =  [
-    {
-        id: 1,
-        name: "João Oliveira",
-        job: "Desenvolvedor"
-    }
-]
-
-
-module.exports = fakeData;
+export default fakeData;
